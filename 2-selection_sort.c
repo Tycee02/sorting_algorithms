f3d9312620f61cf00f9469e5ba5843ab5f643dc4
Tycee02 <wakilaminat@gmail.com>
2024-01-23 19:43:25 +0100

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,51 +1,41 @@
 #include "sort.h"
 
 /**
- * swap - function that swaps the value of two integers
- * @first: pointer to the first integer
- * @second: pointer to the second integer
- **/
-
-void swap(int *first, int *second)
-{
-	int tmp = *first;
-	*first = *second;
-	*second = tmp;
-}
-
-/**
  * selection_sort - function that sorts an array of integers in ascending order
  * using the Selection sort algorithm
  * @array: numbers to be sorted
- * @size: number of i nntegers in array
+ * @size: number of integers in array
  **/
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	size_t c;
+	size_t min_index;
+	int tmp;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		size_t index_min = i;
+		min_index = i;
 
-		for (j = 0; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[index_min])
+			if (array[j] < array[min_index])
 			{
-				index_min = j;
+				min_index = j;
 			}
 		}
 
-		swap(&array[index_min], &array[i]);
-		for (c = 0; c < size; c++)
+		if (min_index != i)
 		{
-			printf("%d", array[c]);
-			if (c < size - 1)
-				printf(", ");
-			else
-				printf("\n");
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+
+			print_array(array, size);
 		}
 	}
 }
