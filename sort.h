ca4092d78646a15d29dcadab312feb24b79e81ea
Tycee02 <wakilaminat@gmail.com>
2024-01-23 19:58:31 +0100

    Header file

--- sort.h
+++ sort.h
@@ -24,6 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int beg, int end, int size);
+void quickSort(int *array, int beg, int end, int size);
 void shell_sort(int *array, size_t size);
 
 #endif
